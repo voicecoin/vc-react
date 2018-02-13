eb6c07a525dd3d1b27b57e32e59a25f220a0187d
Deep-Blue-2013 <deepbluechicago@gmail.com>
2018-02-12 20:52:11 -0600

    fontawesome.css

---
+++ app-web/src/config/i18n.js
@@ -0,0 +1,14 @@
+let strings = new LocalizedStrings({
+  en:{
+    how:"How do you want your egg today?",
+    boiledEgg:"Boiled egg",
+    softBoiledEgg:"Soft-boiled egg",
+    choice:"How to choose the egg"
+  },
+  zh: {
+    how:"Come vuoi il tuo uovo oggi?",
+    boiledEgg:"Uovo sodo",
+    softBoiledEgg:"Uovo alla coque",
+    choice:"Come scegliere l'uovo"
+  }
+ });
